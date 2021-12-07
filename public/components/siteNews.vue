9ca08fe4867f89ac1ea8274203dc0085f7b01d75
ali-arshadi <realgabral@protonmail.com>
2021-12-07 16:01:44 +0330

    befor installing sacc compiler

---
+++ public/components/siteNews.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="site-news">
+      <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h6>
+      <h1>80% off</h1>
+      <h6>+ new arrivals just added</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.site-news {
+  background: black;
+}
+</style>
--- public/pages/index.vue
+++ public/pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="home-page-wrapper">
+      <div class="home-page-intro-wrapper">
+        <h6 class="home-page-intro">Start shopping with confidence</h6>
+      </div>
+      <SiteNews />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style>
+.home-page-wrapper {
+  padding: 35px;
+}
+.home-page-intro-wrapper {
+  text-align: center;
+}
+.home-page-intro {
+  font-weight: 300;
+  font-size: 0.9em;
+}
+</style>
